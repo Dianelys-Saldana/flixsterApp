a0c46e8ea897d118b9bb7cf9fc31a31c3cf1664d
dianelys.saldana <dianelys.saldana@upr.edu>
2020-10-02 23:24:47 -0400

    Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Dianelys Saldaña on 10/2/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
